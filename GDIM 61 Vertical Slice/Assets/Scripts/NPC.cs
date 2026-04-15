e69c28d50eb8409c65c523a08eb2e87a8a03ea48
yngnicole <nicoley5@uci.edu>
2026-04-14 21:19:31 -0700

    npc

--- GDIM 61 Vertical Slice/Assets/Scripts/NPC.cs
+++ GDIM 61 Vertical Slice/Assets/Scripts/NPC.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,24 +8,35 @@ public class NPC : MonoBehaviour
 {
     [SerializeField] float speed = 3f;
     Transform _destination;
+    bool hasArrived = false;
 
+    public event Action OnArrived;
     public void SetDestination(Transform dest)
     {
         _destination = dest;
     }
     void Update()
     {
-        if (_destination == null) return;
+        MoveInCafe();
+    }
+
+    private void MoveInCafe()
+    {
+        if (_destination == null || hasArrived) return;
 
         if (Vector3.Distance(transform.position, _destination.position) > 0.1f)
         {
             Vector3 direction = (_destination.position - transform.position).normalized;
             transform.position += direction * speed * Time.deltaTime;
         }
+        else
+        {
+            hasArrived = true;
+            OnArrived?.Invoke(); 
+        }
 
-        // use translate to make npc go into the cafe and then stop
-        // so startdestination to stopdestination. 
-        //movement into the cafe
+
+
         //movement to walk out the cafe and disappears. 
         //order icon that pops up and disappears once complete. 
     }
