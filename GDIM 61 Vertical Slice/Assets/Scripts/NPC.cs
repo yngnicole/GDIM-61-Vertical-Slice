82042bb6c1d62535017b32c2eac55f8c68d57ab1
yngnicole <nicoley5@uci.edu>
2026-04-14 19:08:21 -0700

    npc move

--- GDIM 61 Vertical Slice/Assets/Scripts/NPC.cs
+++ GDIM 61 Vertical Slice/Assets/Scripts/NPC.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class NPC : MonoBehaviour
 {
     [SerializeField] Transform _npcTransform;
+    [SerializeField] Transform _destination;
+    [SerializeField] public float speed = 3f;
     void Update()
     {
-      // use translate to make npc go into the cafe and then stop
-      // so startdestination to stopdestination. 
-      //movement into the cafe
-      //movement to walk out the cafe and disappears. 
-      //order icon that pops up and disappears once complete. 
+        Vector3 direction = (_destination.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+
+
+        // use translate to make npc go into the cafe and then stop
+        // so startdestination to stopdestination. 
+        //movement into the cafe
+        //movement to walk out the cafe and disappears. 
+        //order icon that pops up and disappears once complete. 
     }
 }
